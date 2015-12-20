7ed3c5af8041e2decb1ec971abad0322d7f0924e
Simon Liang <no.lhr0909@gmail.com>
2015-12-21 02:44:28 +0800

    added basic dynamic content via jQuery

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-  
+$(".write-nav-button").click(function(event) {
+  $(".write-container").show();
+  $(".comments-container").hide();
+  $(".write-nav-button").addClass("active");
+  $(".comments-nav-button").removeClass("active");
 });
+
+$(".comments-nav-button").click(function(event) {
+  $(".write-container").hide();
+  $(".comments-container").show();
+  $(".write-nav-button").removeClass("active");
+  $(".comments-nav-button").addClass("active");
+});
+
+// 打开页面之后直接点一下这个按钮，把初始状态补上
+$(".comments-nav-button").click();
